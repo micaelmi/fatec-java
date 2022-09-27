a0678470cf5623be0c7115dcb9f2eac2a5f36ae8
Micael Miranda <66328408+micaelmi@users.noreply.github.com>
2022-09-26 22:13:53 -0300

    Jogo funcionando 100% !

--- dev/JogoDaVelha/src/jogodavelha/JogoDaVelha.java
+++ dev/JogoDaVelha/src/jogodavelha/JogoDaVelha.java
@@ -1,62 +1,140 @@
 package jogodavelha;
 
-import java.util.Scanner;
 import static jogodavelha.Entradas.*;
 
 public class JogoDaVelha {
     
     public static String[][] boardSquares = new String[3][3];
+    public static boolean end = false;
     public static boolean xTurn = true;
     public static String player = " ";
     public static String column = " ";
     public static int row = 0;
     
-    public static int c;
+    public static int c, r;
     public static boolean error = false;
+    
+    public static int moves = 0;
 
         
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {        
         
-        if(xTurn) player = "X";
-             else player = "O";
-        
-        System.out.println("        JOGO DA VELHA");
         initializeBoard();
-        board();
+        showBoard();
+        printPlayerTurn();
         
-        System.out.println("Vez do " + player);
-        column = entradaTexto("Escolha uma casa para jogar. (Coluna ENTER Linha) Ex: A 1\n");
-        row = sc.nextInt();
-        //System.out.println("Coluna "+ column + " Linha" + row);
         do {
+            playerInput();
             fillBoard();
-            if(error) 
-                System.out.println("Dado inválido");
-        } while(error);
+        }while(end == false);
+        
         
     }
     
     public static void fillBoard() {
-        if(column.equals("A") || column.equals("a")) {
-            c = 0;
-            error = false;
-        } else if(column.equals("B") || column.equals("b")) {
-            c = 1;
-            error = false;
-        } else if(column.equals("C") || column.equals("c")) {
-            c = 2;
-            error = false;
-        } else {
-            error = true;
+            if(error == false) {
+                r = row - 1;
+                if(xTurn) { 
+                    boardSquares[r][c] = "X";
+                } else {
+                    boardSquares[r][c] = "O";
+                }
+            moves++;
+            
+            xTurn = !xTurn;
+            showBoard();
+            tiedTheGame();            
+            winCondition();
+            printPlayerTurn();
+            
+        }
+    }
+    
+    public static void winCondition() {
+        if( "X".equals(boardSquares[0][0]) && "X".equals(boardSquares[0][1]) && "X".equals(boardSquares[0][2]) ||
+            "X".equals(boardSquares[1][0]) && "X".equals(boardSquares[1][1]) && "X".equals(boardSquares[1][2]) ||
+            "X".equals(boardSquares[2][0]) && "X".equals(boardSquares[2][1]) && "X".equals(boardSquares[2][2]) ||
+            "X".equals(boardSquares[0][0]) && "X".equals(boardSquares[1][0]) && "X".equals(boardSquares[2][0]) ||
+            "X".equals(boardSquares[0][1]) && "X".equals(boardSquares[1][1]) && "X".equals(boardSquares[2][1]) ||
+            "X".equals(boardSquares[0][2]) && "X".equals(boardSquares[1][2]) && "X".equals(boardSquares[2][2]) ||
+            "X".equals(boardSquares[0][0]) && "X".equals(boardSquares[1][1]) && "X".equals(boardSquares[2][2]) ||
+            "X".equals(boardSquares[0][2]) && "X".equals(boardSquares[1][1]) && "X".equals(boardSquares[2][0])
+          ) {
+            System.out.println("X venceu!!");
+            end = true;
         }
         
-        if(row < 1 && row > 3) {
-            error = true;
-        } else {
-            error = false;
+        if( "O".equals(boardSquares[0][0]) && "O".equals(boardSquares[0][1]) && "O".equals(boardSquares[0][2]) ||
+            "O".equals(boardSquares[1][0]) && "O".equals(boardSquares[1][1]) && "O".equals(boardSquares[1][2]) ||
+            "O".equals(boardSquares[2][0]) && "O".equals(boardSquares[2][1]) && "O".equals(boardSquares[2][2]) ||
+            "O".equals(boardSquares[0][0]) && "O".equals(boardSquares[1][0]) && "O".equals(boardSquares[2][0]) ||
+            "O".equals(boardSquares[0][1]) && "O".equals(boardSquares[1][1]) && "O".equals(boardSquares[2][1]) ||
+            "O".equals(boardSquares[0][2]) && "O".equals(boardSquares[1][2]) && "O".equals(boardSquares[2][2]) ||
+            "O".equals(boardSquares[0][0]) && "O".equals(boardSquares[1][1]) && "O".equals(boardSquares[2][2]) ||
+            "O".equals(boardSquares[0][2]) && "O".equals(boardSquares[1][1]) && "O".equals(boardSquares[2][0])
+          ) {
+            System.out.println("O venceu!!");
+            end = true;
         }
-        
+    }
+    
+    public static void playerInput() {
+        row = 0;
+        column = "";
+        do {
+            do {
+                try {
+                    row = entradaInteiro("Escolha uma linha. (1, 2 ou 3)\n");
+                    error = false;
+                } catch(Exception e) {
+                    System.out.println(e.getMessage());
+                    error = true;
+                }
+                inputValidation("R");
+            } while(error);
+
+            do {
+                column = entradaTexto("Escolha uma coluna. (A, B ou C)\n");
+                inputValidation("C");
+            }while(error);
+            
+            if(!" ".equals(boardSquares[(row-1)][c])) {
+                System.out.println("Casa ja preenchida, escolha outra");
+                error = true;
+            } else {
+                error = false;
+            }
+        }while(error);
+    }
+    
+    public static void inputValidation(String type) {
+        // VERIFICA O VALOR DA COLUNA, E SE FOR VÁLIDO,
+        // ATRIBUI UM NÚMERO 
+        if(type.equals("C")) {
+            if(column.equals("A") || column.equals("a")) {
+                c = 0;
+                error = false;
+            } else if(column.equals("B") || column.equals("b")) {
+                c = 1;
+                error = false;
+            } else if(column.equals("C") || column.equals("c")) {
+                c = 2;
+                error = false;
+            } else {
+                System.out.println("Coluna invalida");
+                error = true;
+            }
+        //
+        } else if(type.equals("R")) {
+            if(row < 1 || row > 3) {
+                error = true;
+                System.out.println("Linha invalida");
+            } else {
+                error = false;
+            }
+        } else {
+            System.out.println("Tipo invalido");
+        }
     }
     
     public static void initializeBoard() {
@@ -68,7 +146,8 @@ public class JogoDaVelha {
         }
     }
     
-    public static void board() {
+    public static void showBoard() {
+        System.out.println("       JOGO DA VELHA");
         String horizontal ="    +-----+-----+-----+";
         System.out.println("       A     B     C");
         System.out.println(horizontal);
@@ -78,7 +157,24 @@ public class JogoDaVelha {
         System.out.println(horizontal);
         System.out.println("3   |  "+boardSquares[2][0]+"  |  "+boardSquares[2][1]+"  |  "+boardSquares[2][2]+"  |");
         System.out.println(horizontal);
-        System.out.println(" ");
+        System.out.println(" ");    
+        
+        if(xTurn) player = "X";
+             else player = "O";
+    }
+    
+    public static void printPlayerTurn() {
+        if(end != true) {
+            System.out.println("Vez do " + player);
+        }
+    }
+    
+    public static void tiedTheGame() {
+            if(moves >= 8) {
+                System.out.println("Deu velha!!");
+                end = true;
+                System.exit(0);
+            }
     }
     
 }
